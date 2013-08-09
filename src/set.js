8b0c4a672d2c01bcd3d862cdcfbed6b5ba2d627a
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-08-08 20:54:18 -0700

    added set implementation using object

--- src/set.js
+++ src/set.js
@@ -1,16 +1,33 @@
 var makeSet = function(){
-  var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  var set = Object.create(setPrototype); // fix me  ?????????
+  set._storage = {};
+  set._size = -1;
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(addIn){
+  if (!this.contains(addIn)) {
+    this._size++;
+    this._storage[this._size] = addIn;
+  }
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(query){
+  var found = false;
+  for (var key in this._storage) {
+  	if (this._storage[key] === query) {
+  	  found = true;
+  	}
+  }
+  return found;
 };
 
 setPrototype.remove = function(){
+  var results = this._storage[this._size];
+  if (this._size > 0) {
+  	this._size--;
+  }
+  delete this._storage[this._size];
 };
